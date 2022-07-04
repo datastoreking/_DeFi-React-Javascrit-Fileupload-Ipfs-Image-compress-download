b649879d8681193c1a313303e2fc22a02d0cce15
unknown <vladislavcukanovfree@gmail.com>
2022-07-04 18:01:24 +0900

    add every download button

--- public/js/imageReducer.js
+++ public/js/imageReducer.js
@@ -1,14 +1,14 @@
 const MIME_TYPE = "image/png";
 const QUALITY = 0.7;
+var DownloadFileName;
 
 const input = document.getElementById("img-input");
 input.onchange = function (ev) {
   const MAX_WIDTH = document.getElementById("widthSize").value;
   const MAX_HEIGHT = document.getElementById("heightSize").value;
   const file = ev.target.files[0]; // get the file
-  console.log(file)
+  DownloadFileName = file.name;
   const blobURL = URL.createObjectURL(file);
-  console.log(blobURL)
   const img = new Image();
   img.src = blobURL;
   img.onerror = function () {
@@ -23,15 +23,13 @@ input.onchange = function (ev) {
     canvas.width = newWidth;
     canvas.height = newHeight;
     canvas.id = "myCanvas";
-    console.log(canvas.width, canvas.height, canvas.id)
     const ctx = canvas.getContext("2d");
     ctx.drawImage(img, 0, 0, newWidth, newHeight);
-    console.log(img.width)
     canvas.toBlob(
       (blob) => {
         // Handle the compressed image. es. upload or save in local state
-        displayInfo('Original file size:', img.width, img.height);
-        displayInfo('Compressed file size', newWidth, newHeight);
+        displayInfo('Original file size:', img.width, img.height, "onlyInfo");
+        displayInfo('Compressed file size', newWidth, newHeight, "withDownload");
       },
       MIME_TYPE,
       QUALITY
@@ -61,11 +59,29 @@ function calculateSize(img, maxWidth, maxHeight) {
 
 // Utility functions for demo purpose
 
-function displayInfo(label, w, h) {
+function displayInfo(label, w, h, showInfo) {
   const p = document.createElement('p');
   p.innerText = `${label} - Width: ${w} Height: ${h}`;
+  const button = document.createElement('button');
+  button.setAttribute('onclick','download(this)');
+  button.className = "downloadbtn";
+  const a = document.createElement('a');
+  a.className="download";
+  a.innerText = `Download`;
+  button.appendChild(a);
+  if(showInfo == "withDownload"){
+    p.appendChild(button)
+  }
   document.getElementById('imageUploadforReduce').append(p);
 }
+
+function changeHtmlElementId ()
+  {
+     var htmlElement = document.getElementById("myCanvas");
+     htmlElement.id = "myCanvasOld";  // here you can assign new Id
+     var Tag = document.getElementsByClassName("download");
+     Tag[0].className = "downloadOld";
+  }
 
 function readableBytes(bytes) {
   const i = Math.floor(Math.log(bytes) / Math.log(1024)),
@@ -75,9 +91,9 @@ function readableBytes(bytes) {
 }
 
 function download() {
-  var download = document.getElementById("download");
-  download.download = "download.png"
-  var image = document.getElementById("myCanvas").toDataURL("image/png").replace("image/png", "image/octet-stream");
-  download.setAttribute("href", image);
-  console.log(download)
+  var Tag = document.getElementsByClassName("download");
+  var imageLink = document.getElementById("myCanvas").toDataURL("image/png").replace("image/png", "image/octet-stream");
+  Tag[0].download = DownloadFileName;
+  Tag[0].setAttribute("href", imageLink);
+  changeHtmlElementId();
   }
